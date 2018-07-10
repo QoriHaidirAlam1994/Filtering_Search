1c0524c72203b9cd5701037293f9d2c9c8aacfbb
QoriHaidirAlam1994 <qorihaidiralam@rocketmail.com>
2018-07-10 23:04:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-// import logo from './logo.svg';
 import "./App.css";
-// import Galery from './galery';
 
 class App extends Component {
   constructor() {
@@ -32,34 +30,23 @@ class App extends Component {
     let filteredSearch = this.state.data.filter(items => {
       return items.title.indexOf(this.state.search) !== -1;
     });
-    return (
-      <div>
+    return <div>
         <div className="view-school">
           <div className="view-directory course-directory signed-in-directory">
             <div className="container">
               <div className="row search">
-                <div className="col-lg-12 col-md-12 col-xs-12 pull-right">
+                <div className="col-lg-12 col-md-12 col-xs-12">
                   <form>
                     <div className="input-group">
                       <label for="search" className="sr-only">
                         Search
                       </label>
-                      <input
-                        className="form-control search input-lg"
-                        id="search"
-                        type="search"
-                        placeholder="Search"
-                        onChange={this.updateSearch.bind(this)}
-                      />
+                      <input className="form-control search input-lg" id="search" type="search" placeholder="Search" onChange={this.updateSearch.bind(this)} />
                       <span className="input-group-btn">
                         <label for="search-button" className="sr-only">
                           Search
                         </label>
-                        <button
-                          id="search-button"
-                          className="btn search btn-default btn-lg"
-                          type="submit"
-                        >
+                        <button id="search-button" className="btn search btn-default btn-lg" type="submit">
                           <i className="fa fa-search" />
                         </button>
                       </span>
@@ -70,31 +57,34 @@ class App extends Component {
             </div>
 
             <div className="container">
-              <table className="table table-bordered table-hover text-center">
-                <thead>
-                  <tr>
-                    <th className="th-center">No.</th>
-                    <th className="th-center">Video</th>
-                    <th className="th-center">Link</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {filteredSearch.map((items, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>{i + 1}</td>
-                        <td>{items.title}</td>
-                        <td>{items.url}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+              <div className="row ">
+                <table className="table table-bordered table-hover text-center">
+                  <thead>
+                    <tr>
+                      <th className="th-center">No.</th>
+                      <th className="th-center">Video</th>
+                      <th className="th-center">Link</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {filteredSearch.map((items, i) => {
+                      return <tr key={i}>
+                          <td>{i + 1}</td>
+                          <td>{items.title}</td>
+                          <td>{items.url}</td>
+                        </tr>;
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
+
+          <div className="jumbotron text-center">
+            <h1>Welcome !!!</h1>
+          </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
